20abe10f3122eb2ab6c95e8a83ef983e772cab7f
Mirror-Shard <Inncvation@outlook.com>
2021-05-03 22:11:10 +0300

    diagrams

--- Задания/Ex 3.py
+++ Задания/Ex 3.py
@@ -3,11 +3,11 @@ import math
 k = int(input("Введите целое число: "))
 math.sqrt(k)
 
+str = "простое"
+
 for i in range(2, k):
     if k % i == 0:
         str = "не простое"
         break
-    else:
-        str = "простое"
 
 print(str)
